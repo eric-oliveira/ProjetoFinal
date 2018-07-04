ef994f94bea23b0c1f1161ae5dc0de6400174106
pohaeric <oliveiraericm@gmail.com>
2018-07-04 15:13:32 -0300

    Acrescentar arquivo
    Acrescentado arquivo_cadastro_venda.js

---
+++ js/pagina_cadastro_venda.js
@@ -0,0 +1,16 @@
+$(document).ready(function (){
+	$('#valor').change(function(){
+    	$("#cotacao").empty();
+    	$.ajax({
+        type: "GET", 
+        url: "http://projetolkz.com.br/cotacao/USD;EUR",
+        dataType: "json",
+        cache: false,
+        success: function(data) {
+        			for(var i=0; i<data.length; i++){
+        				console.log(data[i]);
+        			}
+        		} 
+    	});
+    })
+});
