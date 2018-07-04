9d7647496e00e435632ae1d17874168dc12ca709
pohaeric <oliveiraericm@gmail.com>
2018-07-04 17:37:12 -0300

    Alteração pagina_cadastro_venda.js

--- js/pagina_cadastro_venda.js
+++ js/pagina_cadastro_venda.js
@@ -1,9 +1,17 @@
 $(document).ready(function (){
 	$('#valor').change(function(){
     	$("#cotacao").empty();
-    	$.get("http://projetolkz.com.br/cotacao/USD", function(resultado){
-			console.log(resultado);
-		})
+    	$.ajax({
+        type: "GET", 
+        url: "http://projetolkz.com.br/cotacao/USD",
+        dataType: "json",
+        cache: false,
+        success: function(data) {
+        			for(var i=0; i<data.length; i++){
+        				console.log(data[i]);
+        			}
+        		} 
+    	});
     })
 });
 
